4f246007abf6febef340a2039eb6ae4eefe7ecbb
DeepakMahto99999 <mahtodeepak239@gmail.com>
2026-08-06 17:48:49 +0530

    modify middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -2,9 +2,15 @@ import jwt from "jsonwebtoken";
 import userModel from "../models/user.model.js";
 
 export const authMiddleware = async (req, res, next) => {
+     console.log("========== AUTH MIDDLEWARE ==========");
+    console.log("Cookies:", req.cookies);
+    console.log("Authorization:", req.headers.authorization); 
+
     const token =
         req.cookies?.token ||
         req.headers.authorization?.split(" ")[1];
+
+         console.log("Token:", token);
 
     if (!token) {
         return res.status(401).json({
@@ -16,7 +22,11 @@ export const authMiddleware = async (req, res, next) => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+        console.log("Decoded:", decoded);
+
         const user = await userModel.findById(decoded.userId);
+
+        console.log("User:", user);
 
         if (!user) {
             return res.status(401).json({
